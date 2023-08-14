a586ecb3d8f346534b34e7376c2c7787cb382bb7
Akintola Rahmat <akintolarahmah@gmail.com>
2023-08-14 23:36:57 +0000

    configuring default authentication

--- mbchat/mbchat/settings.py
+++ mbchat/mbchat/settings.py
@@ -66,8 +66,11 @@ TEMPLATES = [
 ]
 
 REST_FRAMEWORK = {
-    # YOUR SETTINGS
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
 }
 
 WSGI_APPLICATION = "mbchat.wsgi.application"
